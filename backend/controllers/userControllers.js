bf8f074926a832c8047ac4aeeb096dd6d586a8f9
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-03-28 13:01:56 +0100

    added user auth route

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,27 @@
+import asyncHnadler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+// Auth user retrieve token
+// POST /api/users/login
+// Public
+
+const authUser = asyncHnadler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: null
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid Email and/or Password');
+	}
+});
+
+export { authUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcyrpt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
 	{
@@ -25,6 +26,10 @@ const userSchema = mongoose.Schema(
 		timestamps: true
 	}
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcyrpt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
